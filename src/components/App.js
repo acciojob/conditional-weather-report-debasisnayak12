072a603cdc73da7a0010d76fadcb01ac09976f6c
Debasis Nayak <debasishnayak0739@gmail.com>
2024-01-24 12:17:13 +0530

    all done

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,26 @@
 
-import React from "react";
+import React, { useEffect, useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const [weatherInput, setWeatherInput] = useState({temperature:25,condition:"sunny"});
+  const [textColor, setTextColor] = useState("");
+  
+  useEffect(() => {
+    if(weatherInput.temperature > 20){
+      setTextColor("red");
+    }else{
+      setTextColor("blue");
+    }
+  },[weatherInput.temperature])
+
+
   return (
     <div>
-        {/* Do not remove the main div */}
+       <div style={{color:textColor}}>
+        <p>Temperature: {weatherInput.temperature}</p>
+        <p>Temperature: {weatherInput.condition}</p>
+       </div>
     </div>
   )
 }
